dd4a5fc143d04fc1c2c32f6259a587798c447c3c
Charbel Tabet <b00095271@aus.edu>
2025-09-03 15:47:31 +0400

    Feature B added

--- COE420L1-master/Coe420Lab1/src/coe420l1/Hello.java
+++ COE420L1-master/Coe420Lab1/src/coe420l1/Hello.java
@@ -5,7 +5,13 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		featureB();
 
+	}
+	
+	public static void featureB() {
+	    System.out.println("Feature B!");
 	}
 
 }
+	
